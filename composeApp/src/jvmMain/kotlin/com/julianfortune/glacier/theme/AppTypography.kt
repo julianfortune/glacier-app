3c692379db4bc1aca27d21e6395071b0b22952a3
Julian Fortune <juliandfortune@gmail.com>
2026-07-03 14:34:54 -0700

    Using google sans

---
+++ composeApp/src/jvmMain/kotlin/com/julianfortune/glacier/theme/AppTypography.kt
@@ -0,0 +1,48 @@
+package com.julianfortune.glacier.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import glacierapp.composeapp.generated.resources.Res
+import glacierapp.composeapp.generated.resources.google_sans
+import glacierapp.composeapp.generated.resources.google_sans_italic
+import org.jetbrains.compose.resources.Font
+
+
+@Composable
+fun googleSansFontFamily(): FontFamily {
+    return FontFamily(
+        Font(resource = Res.font.google_sans, style = FontStyle.Normal),
+        Font(resource = Res.font.google_sans_italic, style = FontStyle.Italic),
+    )
+}
+
+
+@Composable
+fun AppTypography(): Typography {
+    val fontFamily = googleSansFontFamily()
+
+    val typography = Typography()
+    return Typography(
+        displayLarge = typography.displayLarge.copy(fontFamily = fontFamily),
+        displayMedium = typography.displayMedium.copy(fontFamily = fontFamily),
+        displaySmall = typography.displaySmall.copy(fontFamily = fontFamily),
+
+        headlineLarge = typography.headlineLarge.copy(fontFamily = fontFamily),
+        headlineMedium = typography.headlineMedium.copy(fontFamily = fontFamily),
+        headlineSmall = typography.headlineSmall.copy(fontFamily = fontFamily),
+
+        titleLarge = typography.titleLarge.copy(fontFamily = fontFamily),
+        titleMedium = typography.titleMedium.copy(fontFamily = fontFamily),
+        titleSmall = typography.titleSmall.copy(fontFamily = fontFamily),
+
+        bodyLarge = typography.bodyLarge.copy(fontFamily = fontFamily),
+        bodyMedium = typography.bodyMedium.copy(fontFamily = fontFamily),
+        bodySmall = typography.bodySmall.copy(fontFamily = fontFamily),
+
+        labelLarge = typography.labelLarge.copy(fontFamily = fontFamily),
+        labelMedium = typography.labelMedium.copy(fontFamily = fontFamily),
+        labelSmall = typography.labelSmall.copy(fontFamily = fontFamily),
+    )
+}
