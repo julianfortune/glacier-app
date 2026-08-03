dd8092ee181179450f5ba8fdb999608c05f38a2a
Julian Fortune <juliandfortune@gmail.com>
2026-08-03 11:41:04 -0700

    Cleanup

--- composeApp/src/jvmTestFixtures/kotlin/com/julianfortune/glacier/Database.kt
+++ composeApp/src/jvmTestFixtures/kotlin/com/julianfortune/glacier/Database.kt
@@ -1,14 +1,11 @@
 package com.julianfortune.glacier
 
-import app.cash.sqldelight.db.QueryResult
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import com.julianfortune.glacier.db.Database
 import java.util.*
 
 suspend fun createTestDatabase(): Database {
-    val properties = Properties().apply {
-        setProperty("foreign_keys", "on")
-    }
+    val properties = Properties().apply { put("foreign_keys", "true") }
 
     val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY, properties).also {
         Database.Schema.create(it).await()
